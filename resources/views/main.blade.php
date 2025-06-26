b898ec141105e9c06cd380b3cd7090c6446096b3
ibrahim çağlayan <94143285+ibrahimcaglayaa@users.noreply.github.com>
2025-06-26 03:28:46 +0300

    Theme

---
+++ resources/views/main.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('include.head')
+
+</head>
+
+<body class="index-page">
+
+<header id="header" class="header d-flex flex-column justify-content-center">
+
+    <i class="header-toggle d-xl-none bi bi-list"></i>
+
+    <nav id="navmenu" class="navmenu">
+        <ul>
+            <li><a href="/" class="active"><i class="bi bi-house navicon"></i><span>Anasayfa</span></a></li>
+            <li><a href=""><i class="bi bi-pin-fill"></i><span>Konum Ekle</span></a></li>
+            <li><a href=""><i class="bi bi-card-list"></i><span>Konum Listele</span></a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<main class="main">
+
+
+    <section>
+
+
+    </section>
+
+
+</main>
+
+
+
+<div id="preloader"></div>
+
+<script src="{{ asset('js/main.js') }}"></script>
+
+</body>
+
+</html>
